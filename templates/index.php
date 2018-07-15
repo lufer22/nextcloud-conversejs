e5c170484d9b161ee78ec915c5be58e77c6a80db
lil5 <li@last.nl>
2018-07-15 13:33:57 +0200

    Added the user form to app-settings #2

--- templates/index.php
+++ templates/index.php
@@ -2,19 +2,28 @@
 script('conversejs', 'converse.min');
 script('conversejs', 'init');
 style('conversejs', 'converse.min');
+style('conversejs', 'index');
 ?>
-<form id="conversejs_user_form">
-	<section>
-		<label>JID</label>
-		<br />
-		<input type="text" name="jid" value="<?php p($_['jid']); ?>" />
-	</section>
-	<section>
-		<label>Password</label>
-		<br/>
-		<input type="password" name="pass" placeholder="<?php echo str_repeat('&#8226;', 10); ?>" />
-	</section>
-	<section>
-		<input type="submit" value="Submit" />
-	</section>
-</form>
+
+<div id="app">
+	<div id="app-navigation">
+		<div id="app-settings">
+			<div id="app-settings-header">
+				<button class="settings-button" data-apps-slide-toggle="#app-settings-content">Account</button>
+			</div>
+			<div id="app-settings-content">
+
+				<form id="conversejs_user_form">
+					<label for="conversejs_jid">JID</label>
+					<input id="conversejs_jid" type="text" name="jid" value="<?php p($_['jid']); ?>" />
+					<label for="conversejs_password">Password</label>
+					<input id="conversejs_password" type="password" name="pass" placeholder="<?php if (!empty($_['jid'])) { echo str_repeat('&#8226;', 10); } ?>" />
+					<input type="submit" value="Submit" />
+				</form>
+
+			</div>
+		</div>
+	</div>
+	<div id="app-content">
+	</div>
+</div>
